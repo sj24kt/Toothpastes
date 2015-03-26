595dd55366416b5ad9371c063ef5bc26d9ad4f9b
Sherrie Jones <sj24kt@comcast.net>
2015-03-26 07:48:34 -0600

    initial commit

---
+++ Toothpastes/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  RootViewController.m
+//  Toothpastes
+//
+//  Created by Sherrie Jones on 3/26/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
